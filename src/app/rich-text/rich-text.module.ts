d4e95a541e0b67f095b977220b258dc0941a9ba2
Natsulelou <nutshapon_lelou@hotmail.com>
2020-02-14 16:07:13 +0700

    ui comment

--- src/app/rich-text/rich-text.module.ts
+++ src/app/rich-text/rich-text.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentBoxComponent } from './comment-box/comment-box.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [CommentBoxComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatInputModule
   ],
   exports: [
     CommentBoxComponent
